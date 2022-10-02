5b98955702c3596f898e42f50b8440ea69faf520
Salty Brackets <zallenwhite@gmail.com>
2022-10-01 18:55:54 -0700

    Utility to configure all sprites in the project with proper texture settings.

---
+++ Assets/Scripts/SpriteImporter.cs
@@ -0,0 +1,37 @@
+#if UNITY_EDITOR
+namespace LD51
+{
+    using System.Linq;
+    using UnityEditor;
+    using UnityEngine;
+
+
+    public class SpriteImporter
+    {
+        [MenuItem("Assets/Conform All Sprite Assets")]
+        private static void ConformSpriteTextures()
+        {
+            int count = 0;
+            string[] paths = AssetDatabase.GetAllAssetPaths()
+                .Where(x => 
+                    System.IO.Path.GetExtension(x).ToLower().Contains("png") && x.Contains("Assets/Sprites/"))
+                .ToArray();
+            
+            foreach (string path in paths)
+            {
+                TextureImporter textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
+                if (textureImporter != null)
+                {
+                    textureImporter.filterMode = FilterMode.Point;
+                    textureImporter.spritePixelsPerUnit = 24;
+                    textureImporter.compressionQuality = 0;
+                    AssetDatabase.ImportAsset(path);
+                    count++;
+                }
+            }
+
+            Debug.Log($"Processed and conformed {count} sprites.");
+        }
+    }
+}
+#endif
