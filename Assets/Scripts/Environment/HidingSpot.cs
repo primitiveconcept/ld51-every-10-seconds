ac52cbc6187a7acccb2f8a8d52f3c59967a48785
Salty Brackets <zallenwhite@gmail.com>
2022-10-02 19:10:24 -0700

    More options for hiding:  - Update move speed while hiding.  - Reposition optional.  - Moving beyond bounds of object will auto-unhide.

--- Assets/Scripts/Environment/HidingSpot.cs
+++ Assets/Scripts/Environment/HidingSpot.cs
@@ -5,42 +5,84 @@ namespace LD51
 
     public partial class HidingSpot : MonoBehaviour
     {
-        public bool InUse;
-
+        public PlayerCharacter PlayerCharacter;
+        public float MovementWhileHiding = 1f;
+        public bool RepositionPlayerWhileHiding = false;
         public Vector2 HidingPositionOffset = new Vector2(0, 0.01f);
 
+        private Collider2D _collider;
+        private float originalMovementSpeed;
+
         private Vector2 originalPlayerPosition;
+
+        private Collider2D Collider
+        {
+            get
+            {
+                if (this._collider == null)
+                    this._collider = GetComponent<Collider2D>();
+                return this._collider;
+            }
+        }
+
+        public bool InUse
+        {
+            get { return this.PlayerCharacter != null; }
+        }
+
+
+        public void Update()
+        {
+            if (!this.InUse)
+                return;
+
+            Bounds playerBounds = this.PlayerCharacter.PlayerCollider.bounds;
+            Bounds hidingSpotBounds = this.Collider.bounds;
+
+            if (playerBounds.max.x < hidingSpotBounds.min.x
+                || playerBounds.min.x > hidingSpotBounds.max.x)
+            {
+                UnHide(this.PlayerCharacter);
+            }
+        }
 
 
         public void Hide(PlayerCharacter playerCharacter)
         {
             // Center player on object
-            this.originalPlayerPosition = playerCharacter.transform.position;
-            playerCharacter.transform.position = new Vector2(
-                this.transform.position.x + this.HidingPositionOffset.x,
-                playerCharacter.transform.position.y + this.HidingPositionOffset.y);
+            if (this.RepositionPlayerWhileHiding)
+            {
+                this.originalPlayerPosition = playerCharacter.transform.position;
+                playerCharacter.transform.position = new Vector2(
+                    this.transform.position.x + this.HidingPositionOffset.x,
+                    playerCharacter.transform.position.y + this.HidingPositionOffset.y);    
+            }
             
             playerCharacter.SpriteRenderer.sortingLayerName = "Hiding";
             playerCharacter.SpriteRenderer.color = Color.grey;
 
-            playerCharacter.Movement.Locked = true;
-            
-            this.InUse = true;
+            this.originalMovementSpeed = playerCharacter.Movement.Speed;
+            playerCharacter.Movement.Speed = this.MovementWhileHiding;
+
+            this.PlayerCharacter = playerCharacter;
         }
 
 
         public void UnHide(PlayerCharacter playerCharacter)
         {
-            playerCharacter.transform.position = new Vector2(
-                playerCharacter.transform.position.x,
-                this.originalPlayerPosition.y);
+            if (this.RepositionPlayerWhileHiding)
+            {
+                playerCharacter.transform.position = new Vector2(
+                    playerCharacter.transform.position.x,
+                    this.originalPlayerPosition.y);    
+            }
             
             playerCharacter.SpriteRenderer.sortingLayerName = "Default";
             playerCharacter.SpriteRenderer.color = Color.white;
             
-            playerCharacter.Movement.Locked = false;
-            
-            this.InUse = false;
+            playerCharacter.Movement.Speed = this.originalMovementSpeed;
+
+            this.PlayerCharacter = null;
         }
     }
 }
